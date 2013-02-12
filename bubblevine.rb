1ddb957a822e199b232df7ed4664d09eb97e7e0f
Julian Simioni <julian.simioni@gmail.com>
2013-02-11 21:02:11 -0800

    Get token from ENV

--- bubblevine.rb
+++ bubblevine.rb
@@ -2,7 +2,7 @@ require 'json'
 require 'sinatra'
 require 'open-uri'
 
-require_relative 'config'
+@@token = ENV['INSTAGRAM_TOKEN']
 
 def get_photo_url
 	url = 'https://api.instagram.com/v1/users/self/feed?client_id=c42c61f4ed8d48149c22aa51deacf4f1&access_token='+@@token
