322458eac2fae3b7ae2766139648dec6cd42cf9b
Julian Simioni <julian.simioni@gmail.com>
2013-02-11 20:29:31 -0800

    Initial working backend prototype

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'json'
+require 'sinatra'
+require 'open-uri'
+
+require_relative 'config'
+
+def get_photo_url
+	url = 'https://api.instagram.com/v1/users/self/feed?client_id=c42c61f4ed8d48149c22aa51deacf4f1&access_token='+@@token
+	response = open(url).read
+	response_json = JSON.parse(response)
+	response_json['data'][0]['images']['standard_resolution']['url']
+end
+
+get '/photo' do
+	get_photo_url
+end
---
+++ test.rb
@@ -0,0 +1,3 @@
+require_relative 'app'
+
+puts get_photo_url
