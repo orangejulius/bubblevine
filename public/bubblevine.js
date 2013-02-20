86528c8ec3008e20778792c3b09617ac47cefeff
Julian Simioni <julian.simioni@gmail.com>
2013-02-19 20:28:41 -0800

    Subscribe to pusher event

--- public/bubblevine.js
+++ public/bubblevine.js
@@ -1,5 +1,9 @@
-var bubblevine = function(element) {
+var bubblevine = function(element, username) {
 	$.get('/photo', function(responseText) {
 		element.style.background = 'url(' + responseText + ')';
+	});
+	var channel = pusher.subscribe('my-channel');
+	channel.bind('my-event', function(data) {
+			alert('Received my-event with message: ' + data.message);
 	});
 };
