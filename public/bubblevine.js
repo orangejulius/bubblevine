9245f30c8733955db15f783f4c7492525a48705d
Julian Simioni <julian.simioni@gmail.com>
2013-02-19 20:36:28 -0800

    Put pusher in the right place

--- public/bubblevine.js
+++ public/bubblevine.js
@@ -2,6 +2,7 @@ var bubblevine = function(element, username) {
 	$.get('/photo', function(responseText) {
 		element.style.background = 'url(' + responseText + ')';
 	});
+	var pusher = new Pusher('ce71ccbe68d44c4b14c7');
 	var channel = pusher.subscribe('my-channel');
 	channel.bind('my-event', function(data) {
 			alert('Received my-event with message: ' + data.message);
