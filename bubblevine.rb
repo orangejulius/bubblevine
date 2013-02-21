c9f46b1143903578943b792176239f5e3a7a7095
Julian Simioni <julian.simioni@gmail.com>
2013-02-20 21:38:09 -0800

    Redirect to test instead of feed
    And remove feed

--- bubblevine.rb
+++ bubblevine.rb
@@ -28,18 +28,7 @@ get "/oauth/callback" do
   response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
 
 	@@redis.set(response.user.id, response.access_token)
-  redirect "/feed"
-end
-
-get "/feed" do
-  client = Instagram.client(:access_token => session[:access_token])
-  user = client.user
-
-  html = "<h1>#{user.username}'s recent photos</h1>"
-  for media_item in client.user_recent_media
-    html << "<img src='#{media_item.images.thumbnail.url}'>"
-  end
-  html
+  redirect "/test.html"
 end
 
 def get_photo_url(user_id)
