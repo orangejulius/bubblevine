2ec9d162026d289f2c1625d333d67c3cfd9cc423
Julian Simioni <julian.simioni@gmail.com>
2013-02-21 17:27:27 -0800

    Don't display the image until its fully loaded

--- public/bubblevine.js
+++ public/bubblevine.js
@@ -1,11 +1,19 @@
 var bubblevine = function(element, username) {
 	$.get('/create_realtime_subscription?user_id='+username);
 	$.get('/photo?user_id='+username, function(responseText) {
-		element.style.background = 'url(' + responseText + ')';
+		set_bg_image(element, responseText);
 	});
 	var pusher = new Pusher('ce71ccbe68d44c4b14c7');
 	var channel = pusher.subscribe(username);
 	channel.bind('new-photo', function(data) {
-			element.style.background = 'url(' + data.message + ')';
+		set_bg_image(element, data.message);
 	});
+};
+
+var set_bg_image = function(element, img_url) {
+	var img = new Image();
+	img.onload = function() {
+		element.style.background  = 'url(' +this.src +')';
+	};
+	img.src = img_url;
 };
