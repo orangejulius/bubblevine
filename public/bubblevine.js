3be28e74edee7c3501cd57e3d298259e7f72b674
Julian Simioni <julian.simioni@gmail.com>
2013-04-13 20:59:27 -0700

    Fix some jslint errors

--- public/bubblevine.js
+++ public/bubblevine.js
@@ -1,20 +1,20 @@
-var bubblevine = function(element, username) {
-	username = username.toString();
-	$.get('/photo?user_id='+username, function(responseText) {
-		set_bg_image(element, responseText);
-	});
-	var pusher = new Pusher('ce71ccbe68d44c4b14c7');
-	var channel = pusher.subscribe(username);
-	channel.bind('new-photo', function(data) {
-		set_bg_image(element, data.message);
-	});
-};
-
 var set_bg_image = function(element, img_url) {
 	var img = new Image();
 	img.onload = function() {
-		element.style.background  = 'url(' +this.src +')';
+		element.style.background  = 'url(' + this.src + ')';
 		element.style.backgroundSize = '100%';
 	};
 	img.src = img_url;
+};
+
+var bubblevine = function(element, username) {
+	username = username.toString();
+	$.get('/photo?user_id=' + username, function(responseText) {
+		set_bg_image(element, responseText);
+	});
+	var pusher = new Pusher('ce71ccbe68d44c4b14c7'),
+	channel = pusher.subscribe(username);
+	channel.bind('new-photo', function(data) {
+		set_bg_image(element, data.message);
+	});
 };
