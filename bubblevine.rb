1cc88a21c3ed835067053681fb9ca6cba6dd08d3
Julian Simioni <julian.simioni@gmail.com>
2013-09-24 14:39:06 -0700

    Rearrange routes

--- bubblevine.rb
+++ bubblevine.rb
@@ -23,10 +23,22 @@ Instagram.configure do |config|
   config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
 end
 
+# user facing routes
 get "/" do
 	slim :index
 end
 
+get '/example' do
+	@user_id = session[:user_id]
+	@base_url = ENV['BASE_URL']
+	slim :example
+end
+
+get '/photo' do
+	get_photo_url(params[:user_id])
+end
+
+# oath routes
 get "/instagram/oauth/connect" do
   redirect Instagram.authorize_url(redirect_uri: INSTAGRAM_CALLBACK_URL)
 end
@@ -40,32 +52,8 @@ get "/instagram/oauth/callback" do
   redirect "/example"
 end
 
-get '/example' do
-	@user_id = session[:user_id]
-	@base_url = ENV['BASE_URL']
-	slim :example
-end
-
-def get_photo_url(user_id)
-	access_token = @@redis.get(user_id)
-	client = Instagram.client(access_token: access_token)
-	client.user_recent_media(user_id)[0].images.standard_resolution.url
-end
-
-get '/photo' do
-	get_photo_url(params[:user_id])
-end
-
-def create_realtime_subscription(user_id)
-	callback_url = ENV['BASE_URL'] + 'instagram/realtime_callback'
-	response = Instagram.create_subscription(object: 'user', aspect: 'media',
-                                           callback_url: callback_url,
-                                           object_id: user_id,
-                                           client_id: ENV['INSTAGRAM_CLIENT_ID'],
-                                           verify_token: 'foo')
-end
-
-#this is used to verify the realtime subscription
+# realtime callback routes
+#this is used to verify the realtime subscription during creation
 get '/instagram/realtime_callback' do
 	params['hub.challenge']
 end
@@ -76,4 +64,21 @@ post '/instagram/realtime_callback' do
 	user_id = data[0]['object_id']
 	photo = get_photo_url(user_id)
 	Pusher[user_id].trigger('new-photo', {'message' => photo})
+end
+
+# helper method to get the photo url to use
+def get_photo_url(user_id)
+	access_token = @@redis.get(user_id)
+	client = Instagram.client(access_token: access_token)
+	client.user_recent_media(user_id)[0].images.standard_resolution.url
+end
+
+# helper method to create a new realtime subscription
+def create_realtime_subscription(user_id)
+	callback_url = ENV['BASE_URL'] + 'instagram/realtime_callback'
+	response = Instagram.create_subscription(object: 'user', aspect: 'media',
+                                           callback_url: callback_url,
+                                           object_id: user_id,
+                                           client_id: ENV['INSTAGRAM_CLIENT_ID'],
+                                           verify_token: 'foo')
 end
