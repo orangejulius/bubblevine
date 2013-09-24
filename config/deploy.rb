dd04ba060298556ee5e6a6963e972a97f32eb363
Julian Simioni <julian.simioni@gmail.com>
2013-09-24 14:16:13 -0700

    Add Capistrano config

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+require 'bundler/capistrano'
+require 'capistrano-unicorn'
+
+set :application, "bubblevine"
+set :repository,  "https://github.com/orangejulius/bubblevine.git"
+set :domain, "juliansimioni.com"
+
+role :app, domain
+role :web, domain
+role :db, domain, primary: true
+
+set :user, 'bubblevine'
+set :deploy_to, '/home/bubblevine'
+set :use_sudo, false
+
+after 'deploy:restart', 'unicorn:restart'  # app preloaded
+after 'deploy:finalize_update', 'config:symlink'
+
+namespace :config do
+  desc "Symlink the configuration files for the app"
+  task :symlink do
+    run <<-CMD
+      for i in `ls -A #{shared_path}/config`; do
+        if test -f "#{shared_path}/config/$i"; then
+          ln -nfs "#{shared_path}/config/$i" "#{release_path}/config/$i";
+        fi;
+      done;
+    CMD
+  end
+end
