b00f02c5ac258cf7101ddf4280cdb0e93dcb5844
Julian Simioni <julian.simioni@gmail.com>
2015-02-10 07:21:17 -0800

    Upgrade to latest Pusher version

--- bubblevine.rb
+++ bubblevine.rb
@@ -16,6 +16,10 @@ Instagram.configure do |config|
   config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
 end
 
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
+
 # user facing routes
 get "/" do
 	slim :index
